8a97454ac237e6ac587763467276f50bf20c0183
Mathieu-chaffre <chaffrem@gmail.com>
2020-02-03 09:54:29 +0100

    choix par nom

--- explo.c
+++ explo.c
@@ -6,18 +6,30 @@
 
 char lieu[20]= "montagne";
 char lieu2[20]= "plage";
-int decision=0;
+char decision[20];
+int reponse;
+
+void nom(char * nom1, char * nom2){
+  int reponse;
+reponse = strcmp( lieu, decision );
+
+}
 
 int main(){
   printf("tu peux aller %s ou %s\n", lieu, lieu2);
 printf("Ou veux tu aller ?\n");
-scanf("%d", &decision);
-switch(decision){
-  case 1:
-  printf("tu va  : %s", lieu);
-  break;
-  case 2:
-  printf("Tu va : %s", lieu2);
+scanf("%s", decision);
+
+nom(lieu, decision);
+nom(lieu2, decision);
+
+
+
+if(reponse <0 || reponse  > 0){
+  printf("ce lieu n'existe pas");
+}
+else if (reponse == 0){
+  printf("tu va %s", decision);
 }
 
 
